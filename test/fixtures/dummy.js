367888a73092fa65d43e4c2608ee671b9bb744eb
Rolf Erik Lekang <me@rolflekang.com>
2016-04-16 17:36:22 +0200

    test: Add dummy fixture

---
+++ test/fixtures/dummy.js
@@ -0,0 +1,11 @@
+// this file is used to apply lint failures in while testing
+
+const coffee = '☕'
+
+if (typeof coffee === 'string') {
+  console.log('\o/')
+}
+
+console.log('----------')
+
+return coffee;
