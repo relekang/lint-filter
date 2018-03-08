d979c967707e1b68e9b7417d36283c284ef40115
Max Nowack <max@unsou.de>
2018-03-08 13:25:28 +0100

    Add tests

--- test/parser_tests.js
+++ test/parser_tests.js
@@ -40,6 +40,13 @@ const parseStringResult = [
     source: 'eslint.rules.semi',
     file: 'lint-filter/src/index.js',
   },
+  { line: '7',
+    column: '23',
+    severity: 'error',
+    message: 'Extra semicolon. (semi)',
+    source: 'eslint.rules.semi',
+    file: 'lint-filter/src/index.js',
+  },
 ]
 
 test.serial('parseString(str) should parse xml and make paths relative', async t => {
