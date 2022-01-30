66c97ccfd49cc77ee02bd22fa9a32f3e61743580
Rolf Erik Lekang <me@rolflekang.com>
2022-01-30 10:19:56 +0100

    chore: Use eslint-config-relekang

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
-  extends: "airbnb-base",
-  parser: "babel-eslint",
-  rules: {
-    semi: [2, "never"],
-    "generator-star-spacing": 0
-  }
-}
+  extends: ['relekang', 'relekang/configs/flowtype'],
+  parser: '@babel/eslint-parser',
+  env: {
+    node: true,
+  },
+  settings: {
+    'eslint-config-relekang': {
+      babel: true,
+      typescript: false,
+      react: false,
+      jest: false,
+    },
+  },
+  overrides: [{ files: ['test/**'], rules: { 'no-useless-escape': 'off' } }],
+};
